0e65095135a3d0c1e37ad25560fe338ba306d050
Oscar Buritica <oscar.1701311658@ucaldas.edu.co>
2022-11-27 00:50:59 -0500

    003 : Create API Layer and Unit Test Project

---
+++ NewshoreAirTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace NewshoreAirTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
