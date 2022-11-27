277317077d63ae45198e04a836200fda6a61784a
Oscar Buritica <oscar.1701311658@ucaldas.edu.co>
2022-11-27 01:29:22 -0500

    006: Create Models

---
+++ Domain/Models/Flight.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Flight
+    {
+        public string Origin {get;set;}
+        public string Destination { get;set;}
+        public double Price { get; set; }
+        public Transport Transport { get; set; }
+
+    }
+}
---
+++ Domain/Models/Journey.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Journey
+    {
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public double Price { get; set; }
+        public List<Flight> Flights { get; set; }
+    }
+}
